9b8d223339ac17a0319446f207ce12e1956d4ed4
Jama Mohamed <jama3137@gmail.com>
2019-01-16 23:42:01 +0300

    final

--- q2/appQ2.py
+++ q2/appQ2.py
@@ -8,4 +8,8 @@ class Ship:
 
 if __name__ == '__main__':
     titanic = Ship(15, 10)
-    print(titanic.is_worth_it())
+    flagShip = Ship(200, 40)
+    victory = Ship(2, 1)
+    print('The titanic is a {}'.format(titanic.is_worth_it()))
+    print('The victory is a {}'.format(victory.is_worth_it()))
+    print('The flagShip is a {}'.format(flagShip.is_worth_it()))
--- q4/appQ4.py
+++ q4/appQ4.py
@@ -1,13 +1,13 @@
 import random
 import time
 
-def loader():
+def loader(value):
     ticks = 0;
     while True:
         print("*")
         time.sleep(1)
         ticks += 1
-        if ticks >= 3:
+        if ticks >= value:
             break
 
 def getInput(text):
@@ -16,7 +16,7 @@ def getInput(text):
 
 def answer():
     responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes-definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-    loader()
+    loader(5)
     return print(random.choice(responses))
 
 def askAgain():
