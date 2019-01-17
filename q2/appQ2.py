9d772665f93f75bf9adeb63e1ab61946ae8362f5
Jama Mohamed <jama3137@gmail.com>
2019-01-17 09:25:22 +0300

    final

--- q2/appQ2.py
+++ q2/appQ2.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     titanic = Ship(15, 10)
     flagShip = Ship(200, 40)
     victory = Ship(2, 1)
+    
     print('The titanic is a {}'.format(titanic.is_worth_it()))
     print('The victory is a {}'.format(victory.is_worth_it()))
     print('The flagShip is a {}'.format(flagShip.is_worth_it()))
--- q4/appQ4.py
+++ q4/appQ4.py
@@ -4,6 +4,7 @@ import time
 def loader(value):
     ticks = 0;
     while True:
+        # Loading icon
         print("*")
         time.sleep(1)
         ticks += 1
@@ -13,19 +14,18 @@ def loader(value):
 def getInput(text):
     return input(text)
 
-
 def answer():
     responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes-definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+    # Start the loader
     loader(5)
     return print(random.choice(responses))
 
 def askAgain():
     while True:
-        responses = getInput("Would you like to try again? (Y/N) ")
-        changeToUpper = responses.upper()
-        if changeToUpper == "Y":
+        response = getInput("Would you like to try again? (Y/N) ").upper()
+        if response == "Y":
             return True
-        elif changeToUpper == "N":
+        elif response == "N":
             return False
         else:
             print("Please enter Y or N")
@@ -33,11 +33,12 @@ def askAgain():
 
 def start():
     while True:
-        responses = getInput("What is your question? ")
+        getInput("What is your question? ")
         answer()
         if askAgain() == False:
             break
     return print("Have a good day!")
 
 if __name__ == '__main__':
+    # Start the program
     start()
